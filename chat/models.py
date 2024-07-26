b4a0e20fc2cf126cadcca4d20efd57b4fb1f7c05
limntrix <v.k.nagar100@gmail.com>
2024-07-26 23:02:04 +0530

    edufreemain

---
+++ chat/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from account.models import User 
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    group_number = models.TextField()
+    def __str__(self):
+        return self.group_number
